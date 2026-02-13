fc3d833fe20d51c0749875de3ea3efa3e0e99636
adebayo-victor <adebayovictorvicade@gmail.com>
2026-02-13 23:27:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -517,6 +517,8 @@ def dashboard():
         event_datetime = event['date']
         
         # Check if the event date is in the past
+        format_of_input = "%Y-%m-%d %H:%M:%S"
+        event_datetime = datetime.strptime(event_datetime, format_of_input)
         diff = datetime.now().date() - event_datetime
         if int(diff.days) >= 2:
             # This prevents directory traversal attacks
